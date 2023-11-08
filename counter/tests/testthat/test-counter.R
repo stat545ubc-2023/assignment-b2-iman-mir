c1bc3b3bad2fa2b7fca19ab85ba231e7b8bd587d
Iman Mir <imanmir@student.ubc.ca>
2023-11-07 16:49:38 -0800

    update

--- counter/tests/testthat/test-counter.R
+++ counter/tests/testthat/test-counter.R
@@ -1,31 +1,28 @@
-# Loading libraries for answer variables
-library(datateachr)
-
 # Test #1 -> Groups without NA
-answer_1 <- vancouver_trees %>%
+answer_1 <- datateachr::vancouver_trees %>%
   dplyr::group_by(species_name ) %>%
   dplyr::summarise(n = n())
 
 test_that("Test that counter works with group with no NA", {
-  expect_equal(counter(vancouver_trees, species_name), answer_1)
+  expect_equal(counter(datateachr::vancouver_trees, species_name), answer_1)
 })
 
 
 # Test #2 -> Groups with NA
-answer_2 <- vancouver_trees %>%
+answer_2 <- datateachr::vancouver_trees %>%
   dplyr::group_by(longitude ) %>%
   dplyr::summarise(n = n())
 
 test_that("Test that counter works with group with  NA", {
-  expect_equal(counter(vancouver_trees, longitude), answer_2)
+  expect_equal(counter(datateachr::vancouver_trees, longitude), answer_2)
 })
 
 
 # Test #3 -> Groups of different type (date format)
-answer_3 <- vancouver_trees %>%
+answer_3 <- datateachr::vancouver_trees %>%
   dplyr::group_by(date_planted ) %>%
   dplyr::summarise(n = n())
 
 test_that("Test that counter works with group with  NA", {
-  expect_equal(counter(vancouver_trees, date_planted), answer_3)
+  expect_equal(counter(datateachr::vancouver_trees, date_planted), answer_3)
 })
